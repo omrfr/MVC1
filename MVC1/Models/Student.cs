0c4ae1a7fe4e5129482063407918979b9ef0b3f5
Ömer Faruk Temir <201118054@samsun.edu.tr>
2022-11-27 18:56:00 +0300

    Add project files.

---
+++ MVC1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+    }
+}
